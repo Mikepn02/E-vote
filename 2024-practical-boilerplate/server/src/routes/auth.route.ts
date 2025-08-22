b450d62abbe614cbab54615f1e4708cfa259e256
mikepn02 <nzaberamikepeter@gmail.com>
2025-08-22 12:20:36 +0200

    Purge boilerplate files, retain core project structure, and finalize initial implementation

--- 2024-practical-boilerplate/server/src/routes/auth.route.ts
+++
@@ -1,9 +0,0 @@
-import express from 'express'
-import * as authController from '../controllers/auth.controller'
-
-const router = express.Router() 
-
-router.post('/login', authController.login)
-router.post('/register', authController.register)
-
-export { router as authRoute }
--- RestTemplate/backend/src/cron/index.ts
+++
@@ -1,7 +0,0 @@
-// import cron from "node-cron";
-// import { task } from "./task";
-
-// cron.schedule("* * * * *", async () => {
-//   console.log("Running scheduled task: task every minute");
-//   await task();
-// });
